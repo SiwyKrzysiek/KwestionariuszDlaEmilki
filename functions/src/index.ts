2bc50840a36cb1f4117d607845f91e87e9e29d3b
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2022-01-22 16:01:40 +0100

    Try to use cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,22 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { TypeCount } from './dto';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+export const questionersLeft = functions.https.onRequest(async (req, res) => {
+    const typeCountDoc = await admin.firestore()
+        .collection('toCreate')
+        .doc("typeCount")
+        .get();
+
+    const typeCount = typeCountDoc.data() as TypeCount;
+    res.json(typeCount);
+});
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
