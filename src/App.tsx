1c543624a69c0ae1d7e2b9b4dc5e4f01019972c5
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2022-01-22 16:52:13 +0100

    Mechanism for taking random form

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { TypeCount } from '../functions/src/dto/TypeCount';
-import { questionersLeft } from './firebase/cheapFunctions';
+import { getQuestioner, questionersLeft } from './firebase/cheapFunctions';
 
 function App() {
   const [count, setCount] = useState(0)
   const [leftCount, setLeftCount] = useState<TypeCount | undefined>();
 
+  const [receivedQuestioner, setReceivedQuestioner] = useState<string | null | undefined>();
+
+  const fetchLeftCount = async (): Promise<void> => {
+    const data = await questionersLeft();
+    setLeftCount(data);
+  }
+
+  const takeRandomForm = (): Promise<void> =>
+    getQuestioner()
+      .then(setReceivedQuestioner)
+      .then(fetchLeftCount);
+
   useEffect(() => {
-    questionersLeft().then(setLeftCount);
+    fetchLeftCount();
   }, []);
 
   return (
@@ -25,6 +37,17 @@ function App() {
         <p>
           Press button if you are excited ^^
         </p>
+        <p>
+          <button type="button" onClick={takeRandomForm}>
+            Weź formularz
+          </button>
+        </p>
+        {receivedQuestioner !== undefined &&
+        <p>
+          {receivedQuestioner
+            ? `Wypełnij formularz ${receivedQuestioner}`
+            : 'Nie ma już więcej formularzy :('}
+        </p>}
       </header>
       <main>
         {JSON.stringify(leftCount)}
--- src/firebase/cheapFunctions/index.ts
+++ src/firebase/cheapFunctions/index.ts
@@ -3,7 +3,7 @@
 
 import { firestore, toCreateCollection } from './../index';
 import { TypeCount } from '../../../functions/src/dto/TypeCount';
-import { doc, getDocFromServer } from '@firebase/firestore';
+import { doc, getDocFromServer, increment, updateDoc } from '@firebase/firestore';
 
 export const questionersLeft = async (): Promise<TypeCount> => {
     const docRef = doc(firestore, toCreateCollection.id, "typeCount");
@@ -12,4 +12,29 @@ export const questionersLeft = async (): Promise<TypeCount> => {
         throw new Error("No required document found");
 
     return docSnap.data() as TypeCount;
+}
+
+export const getQuestioner = async () : Promise<string | null> => {
+    const lefCount = questionersLeft();
+    const counts = Object.values(lefCount) as number[];
+
+    if (counts.every(c => c <= 0))
+        return null;
+
+    const takeOne = async (key: string): Promise<string> => {
+        const document = doc(firestore, toCreateCollection.id, "typeCount");
+        await updateDoc(document, {
+            key: increment(-1)
+        });
+        return key;
+    }
+
+    const maxValue = Math.max(...counts);
+    for (const [k, v] of Object.entries(lefCount)) {
+        if (v === maxValue) {
+            await takeOne(k)
+        }
+    }
+
+    return null;
 }
